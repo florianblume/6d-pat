f04b28d50b7137f726d53f37278a4500ae8d6e15
Florian Blume <flob.91@gmx.de>
2021-07-01 21:06:03 +0200

    Some small finishing touches

--- src/view/settings/settingsloadsavepage.cpp
+++ src/view/settings/settingsloadsavepage.cpp
@@ -2,6 +2,7 @@
 #include "ui_settingsloadsavepage.h"
 #include "view/misc/displayhelper.hpp"
 #include "misc/global.hpp"
+#include "view/misc/displayhelper.hpp"
 
 #include <QFileDialog>
 #include <QtDebug>
@@ -11,6 +12,8 @@ SettingsLoadSavePage::SettingsLoadSavePage(QWidget *parent) :
     ui(new Ui::SettingsLoadSavePage) {
     ui->setupUi(this);
     DisplayHelper::setIcon(ui->buttonPythonScript, fa::folderopen, 20);
+    DisplayHelper::setIcon(ui->buttonPythonScriptHelp, fa::infocircle, 20);
+    DisplayHelper::setIcon(ui->buttonDefaultJsonHelp, fa::infocircle, 20);
 }
 
 SettingsLoadSavePage::~SettingsLoadSavePage() {
@@ -48,6 +51,31 @@ void SettingsLoadSavePage::buttonPythonScriptClicked() {
         settings->setLoadSaveScriptPath(newPath);
         ui->radioButtonPythonScript->setChecked(true);
     }
+}
+
+void SettingsLoadSavePage::buttonDefaultJsonHelpClicked() {
+    QString title = "Default JSON loader for data";
+    QString message = "This is the default JSON loader which expects your data (i.e. the"
+                      "camera info file info.json and ground truth file e.g. gt.json)"
+                      "to be in a certain format, i.e. JSON. It also expects the info.json"
+                      "file to be located in the images folder. Checkout the GitHub page"
+                      "to see what format the files have to conform to.";
+    std::unique_ptr<QMessageBox> messageBox = DisplayHelper::messageBox(
+                this, QMessageBox::Information, title, message, "OK", QMessageBox::AcceptRole);
+    messageBox->exec();
+}
+
+void SettingsLoadSavePage::buttonPythonScriptHelpClicked() {
+    QString title = "Dynamic Python script data loader";
+    QString message = "This option allows you to specify a Python script that loads the"
+                      "data. It is required to have a load_images, load_object_models, "
+                      "load_poses and persist_pose function with certain parameters ("
+                      "checkout the GitHub page to see what parameters excatly and what"
+                      "return types are expected from the script). This way, dynamic"
+                      "data loading is possible wihtout the need for conversion beforehand.";
+    std::unique_ptr<QMessageBox> messageBox = DisplayHelper::messageBox(
+                this, QMessageBox::Information, title, message, "OK", QMessageBox::AcceptRole);
+    messageBox->exec();
 }
 
 QString SettingsLoadSavePage::openFileDialogForPath(QString path) {
